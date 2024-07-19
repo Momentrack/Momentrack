57ba9418f78d18786516ba89c392393b3166cc09
Nat <cestbonciel88@knou.ac.kr>
2024-07-20 04:52:15 +0900

    Design: 플로팅버튼 UI 테스트

--- Momentrack/Controller/HomeViewController.swift
+++ Momentrack/Controller/HomeViewController.swift
@@ -18,6 +18,7 @@ final class HomeViewController: UIViewController {
         
         setupNavigationBar()
         setupView()
+        setupBlurEffect()
     }
     
     private func setupNavigationBar() {
@@ -48,11 +49,38 @@ final class HomeViewController: UIViewController {
         navigationController?.navigationBar.tintColor = .black
     }
     
+    private let floattingButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "floating_90"), for: .normal)
+        return button
+    }()
+    
+    lazy var blurView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.lightGray.withAlphaComponent(0.1)
+        return view
+    }()
+    
+    func setupBlurEffect() {
+        let blurEffect = UIBlurEffect(style: .systemChromeMaterialDark)
+        let blur2 = UIBlurEffect(style: .regular)
+        let vibrancyEffect = UIVibrancyEffect(blurEffect: blur2)
+        let visualEffectView = UIVisualEffectView(effect: blurEffect)
+//        let visualEffectView = UIVisualEffectView(effect: vibrancyEffect)
+        visualEffectView.frame = view.frame
+        blurView.addSubview(visualEffectView)
+        
+    }
+    
     private func setupView() {
         self.view.backgroundColor = .white
         self.view.addSubview(friendListView)
         self.view.addSubview(todayDateView)
         self.view.addSubview(momentListView)
+//        self.view.addSubview(floattingButton)
+        view.addSubview(floattingButton)
+        view.addSubview(blurView)
+        view.bringSubviewToFront(floattingButton)
         
         friendListView.snp.makeConstraints { make in
             make.top.equalTo(self.view.safeAreaLayoutGuide).offset(15)
@@ -69,5 +97,41 @@ final class HomeViewController: UIViewController {
             make.left.right.equalToSuperview()
             make.bottom.equalTo(self.view.safeAreaLayoutGuide)
         }
+
+        floattingButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(16)
+        }
+        
+        blurView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.width.equalTo(view.snp.width)
+            $0.top.equalToSuperview().inset(700)
+        }
+        
     }
 }
+
+// MARK: - Preview Setting
+
+#if DEBUG
+import SwiftUI
+struct HomeViewPreview: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> some UIViewController {
+       HomeViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        
+    }
+}
+
+struct HomeViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        Group {
+            HomeViewPreview()
+        }
+    }
+}
+#endif
+

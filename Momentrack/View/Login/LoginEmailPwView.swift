204cd355682f69f4b8a4639a6cf123ce285a1a95
Nat <cestbonciel88@knou.ac.kr>
2024-08-03 21:17:27 +0900

    Design: 이메일, 비번 로그인 UI

---
+++ Momentrack/View/Login/LoginEmailPwView.swift
@@ -0,0 +1,19 @@
+//
+//  LoginEmailPwView.swift
+//  Momentrack
+//
+//  Created by Nat Kim on 8/3/24.
+//
+
+import UIKit
+
+final class LoginEmailPwView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: .zero)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
